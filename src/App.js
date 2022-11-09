e675a1097521a7a247f7baff7d1fb2aeb76adfda
BitTracers SDM <bittracer.v1@gmail.com>
2022-11-09 23:41:08 +0600

    add basic router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
-function App() {
+import Home from "./views/home";
+import Build from "./views/build";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename="/">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/build" element={<Build />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/views/build/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './build.scss';
+
+class Build extends Component {
+  render() {
+    return (
+      <div className='build'>
+        <div className='title'>🅂🅃🄰🅁🅃 🄷🄴🅁🄴</div>
+      </div>
+    );
+  }
+}
+
+export default Build;
